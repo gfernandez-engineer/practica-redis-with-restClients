9c527035a0fd54a67b9a7243bf1df8c96f3ddbb7
Giancarlo Fernandez <giancarlo.fernandez@outlook.com>
2024-11-12 12:06:19 -0500

    adding resttemplate project

---
+++ sistema-resttemplate/src/main/java/com/practica/sistema_resttemplate/SistemaResttemplateApplication.java
@@ -0,0 +1,13 @@
+package com.practica.sistema_resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaResttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaResttemplateApplication.class, args);
+	}
+
+}
---
+++ sistema-resttemplate/src/test/java/com/practica/sistema_resttemplate/SistemaResttemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.sistema_resttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaResttemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
